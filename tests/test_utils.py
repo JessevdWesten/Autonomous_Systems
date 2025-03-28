1759dee5001e20860ab5727bdabd52859059661e
JessevdWesten <22075208@student.hhs.nl>
2025-03-28 22:31:50 +0100

    nieuw

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,19 +1,15 @@
 import sys
 import os
+import pytest
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 from src.utils import BlackjackStrategy
-import pytest
 
 class TestBlackjackStrategy:
     @pytest.fixture
     def strategy(self):
         return BlackjackStrategy()
-
-    def test_card_counting(self, strategy):
-        strategy.count_cards('5')
-        strategy.count_cards('K')
-        assert strategy.HI_LO_COUNT == 0
 
     def test_basic_strategy(self, strategy):
         test_cases = [
@@ -26,12 +22,5 @@ class TestBlackjackStrategy:
         ]
         
         for dealer_card, player_total, usable_ace, expected in test_cases:
-            action, _ = strategy.get_optimal_move(10, dealer_card, player_total, usable_ace)
+            action = strategy.get_optimal_move(dealer_card, player_total, usable_ace)
             assert action == expected
-
-    def test_bet_calculation(self, strategy):
-        strategy.HI_LO_COUNT = 3
-        assert strategy.calculate_bet(10) == 20
-        
-        strategy.HI_LO_COUNT = -3
-        assert strategy.calculate_bet(10) == 5
